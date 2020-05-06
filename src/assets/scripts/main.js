1950cb56f83bfac7e1465d7ef1dc17c2ee1492e2
Sebastian Vila Ruano <svilaru@uoc.edu>
2020-05-06 12:48:04 +0200

    First version

---
+++ src/assets/scripts/main.js
@@ -0,0 +1,18 @@
+/**
+ * Import dependencies from node_modules
+ * see commented examples below
+ */
+import 'jquery';
+import 'bootstrap';
+// import 'some-node-module';
+// import SomeModule from 'some-node-module';
+
+/**
+ * Write any other JavaScript below
+ */
+
+
++( function() {
+  console.log('Hello, UOC!');
+} )();
+
