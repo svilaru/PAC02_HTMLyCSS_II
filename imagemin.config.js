b2ac1e8062365935611511312cd3ff176bc7c633
Sebastian Vila Ruano <svilaru@uoc.edu>
2020-05-08 15:26:51 +0200

    Fixed the image quality

--- imagemin.config.js
+++ imagemin.config.js
@@ -1,12 +1,9 @@
 module.exports = {
-  "gifsicle": { "optimizationLevel": 2, "interlaced": false, "colors": 10 },
-  "mozjpeg": { "progressive": true, "quality": 10 },
-  "pngquant": { "quality": [0.25, 0.5] },
-  "svgo": {
-    "plugins": [
-      { "removeViewBox": false },
-      { "cleanupIDs": true },
-    ]
+  gifsicle: { optimizationLevel: 2, interlaced: false, colors: 10 },
+  mozjpeg: { progressive: true, quality: 60 },
+  pngquant: { quality: [0.25, 0.5] },
+  svgo: {
+    plugins: [{ removeViewBox: false }, { cleanupIDs: true }],
   },
-  "webp": { "quality": 10 }
-}
+  webp: { quality: 60 },
+};
